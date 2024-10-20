3f1f687bf7c2543ce3b65d9382f86e9a8dca162b
samharrisMQ <samuel.harris2@students.mq.edu.au>
2024-10-20 20:05:27 +1100

    added main class, method and comments for future use

---
+++ Main.java
@@ -0,0 +1,18 @@
+// / basic understanding
+// // know how to use
+// /// can use comfortably
+// no understanding
+//
+//to study:
+// streams e.g. map(), filter(), collect()
+// lambda
+// collectors
+// filde handling e.g. Files.lines()
+// HashMap/Map
+// java date/time handling e.g. LocalDate
+
+class Main {
+    public static void main(String[] args) {
+        
+    }
+}
